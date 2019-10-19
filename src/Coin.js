68069f42e1abbcfcfa6f9c45621290b356e2dcae
Tye Campbell <tylerjoelcampbell@gmail.com>
2019-10-19 08:51:15 -0500

    adjusted the alt description to dynamically display the image

--- src/Coin.js
+++ src/Coin.js
@@ -6,7 +6,7 @@ import './Coin.css';
 class Coin extends Component {
     render() {
         return (
-            <img className='Coin-img' src={this.props.face} alt="coin image"/> 
+            <img className='Coin-img' src={this.props.face} alt={`coin showing ${this.props.face}`}/> 
         )
     }
 }
