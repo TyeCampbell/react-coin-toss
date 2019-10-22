b529f9555660e78fe512df3fde04b2689ea5b77b
Tye Campbell <tylerjoelcampbell@gmail.com>
2019-10-22 06:10:58 -0500

    working on the flip animation and getting it to apply with React

--- src/CoinToss.js
+++ src/CoinToss.js
@@ -13,6 +13,7 @@ class CoinToss extends Component {
     constructor(props){
         super(props);
         this.state = {
+            flipAnimaiton: false,
             frontFace: this.randomCoinFace(),
             backFace: this.randomCoinFace(),
             flips: 0, 
@@ -30,18 +31,20 @@ class CoinToss extends Component {
 
     toss() {
         
+        this.setState({flipAnimaiton: true});
+
         const face = this.randomCoinFace();
 
         console.log(face);
 
-        if (face == heads) {
+        if (face === heads) {
             this.setState({
                 frontFace: this.props.coinFace[0],
                 backFace: this.props.coinFace[1],
                 heads: this.state.heads += 1,
                 flips: this.state.flips += 1,
             })
-        } else if (face == tails) {
+        } else if (face === tails) {
             this.setState({
                 frontFace: this.props.coinFace[1],
                 backFace: this.props.coinFace[0],
@@ -65,7 +68,7 @@ class CoinToss extends Component {
             <div className="CoinToss">
                 <h1>Coin Toss</h1>
                 <div className='flip-coin'>
-                    <div className='flip-coin-inner'>
+                    <div className={`flip-coin-inner ${this.state.flipAnimation === true ? 'flip-animation' : ''}`}>
                         <div className='flip-coin-front'>
                             <Coin face={this.state.frontFace} /> 
                         </div>
