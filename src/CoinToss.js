7e254da6f5173ffa0c99253a9f2a1a7760648c1e
Tye Campbell <tylerjoelcampbell@gmail.com>
2019-10-21 20:56:41 -0500

    changed state to include a backside image of the opposite coin head. changed the JSX to feature the divs needed to implement a flip feature.

--- src/CoinToss.js
+++ src/CoinToss.js
@@ -13,7 +13,8 @@ class CoinToss extends Component {
     constructor(props){
         super(props);
         this.state = {
-            face: this.randomCoinFace(),
+            frontFace: this.randomCoinFace(),
+            backFace: this.randomCoinFace(),
             flips: 0, 
             heads: 0,
             tails: 0,
@@ -33,15 +34,20 @@ class CoinToss extends Component {
 
         console.log(face);
 
-        this.setState({
-            face: face,
-            flips: this.state.flips += 1,
-        })
-
         if (face == heads) {
-            this.setState({heads: this.state.heads += 1})
+            this.setState({
+                frontFace: this.props.coinFace[0],
+                backFace: this.props.coinFace[1],
+                heads: this.state.heads += 1,
+                flips: this.state.flips += 1,
+            })
         } else if (face == tails) {
-            this.setState({tails: this.state.tails += 1})
+            this.setState({
+                frontFace: this.props.coinFace[1],
+                backFace: this.props.coinFace[0],
+                heads: this.state.tails += 1,
+                flips: this.state.flips += 1,
+            })
         }
     }
 
@@ -58,8 +64,15 @@ class CoinToss extends Component {
         return (    
             <div className="CoinToss">
                 <h1>Coin Toss</h1>
-                <div>
-                   <Coin face={this.state.face} /> 
+                <div className='flip-coin'>
+                    <div className='flip-coin-inner'>
+                        <div className='flip-coin-front'>
+                            <Coin face={this.state.frontFace} /> 
+                        </div>
+                        <div className='flip-coin-back'>
+                            <Coin face={this.state.backFace} /> 
+                        </div>
+                    </div>
                 </div>
                 <button onClick={this.toss}>Toss it!</button>
                 <button onClick={this.reset}>Reset</button>
